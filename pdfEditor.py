2f9e20680ffa5e9f24cdafb74d354f5d660e03e3
wloeffler <will@wloeffler.com>
2020-08-18 19:31:48 -0400

    pdf creator multiline support added and problems fixed (dont use % when you need to use //)
    XML needs more work and research to refine selections

--- pdfEditor.py
+++ pdfEditor.py
@@ -89,19 +89,82 @@ for x in range(0, fullFilePath.__len__()):
 
     tempFile = open('temp.txt','w')
     tempFile.write(text)
+    '''
+    for line in text:
+        print(len(line))
+        if len(line)>84:
+            print('called')
+            iterations = len(line) % 84
+            for ix in range(0, iterations):
+                tempFile.write(line[int(ix * 84): int(((ix + 1) * 84))] + '')
+        else:
+            tempFile.write(line)
+'''
+
     tempFile.close()
 
     readinFile = open('temp.txt')
+    pdfLineSize = 84
+
 
     if(flag):
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Arial", size=8)
-        outfile = open('testfile.pdf', 'w')
+
+        pdf = FPDF(orientation= 'P', unit= 'mm', format='letter')
+        pdf.set_right_margin(margin='10')
+        pdf.add_page(orientation='P')
+        #pdf.accept_page_break()
+        pdf.set_font("Courier", size=10)
+        outfile = open('testfile1.pdf', 'w')
+        lineNum =0
         for line in readinFile:
-            pdf.cell(200,10, txt=line, ln=1, align='l')
-            print(line)
-        pdf.output('testfile.pdf')
+            # adds support for lines greater than x char
+            print(f"called  {len(line)}")
+            tempText = line.replace('\n', '')
+            tempText = tempText.replace('\r', '')
+            tempText = tempText.replace('\t', '')
+            iterations = len(line) // pdfLineSize
+            if(iterations == 0):
+                iterations =1
+            for ix in range(0, iterations):
+                if('MRN' in line and lineNum != 0):
+                    pdf.add_page()
+                pdf.cell(pdfLineSize, 3.5, txt=tempText[int(ix * pdfLineSize):int(((ix + 1) * pdfLineSize))], ln=1,
+                         align='l')
+                lineNum = lineNum+1
+
+        '''
+        
+        pdf = FPDF(orientation= 'P', unit= 'mm', format='letter')
+        pdf.set_right_margin(margin='10')
+        pdf.add_page(orientation='P')
+        #pdf.accept_page_break()
+        pdf.set_font("Courier", size=7)
+        outfile = open('testfile1.pdf', 'w')
+        for line in readinFile:
+            #adds support for lines greater than x char
+            if(len(line)>pdfLineSize):
+                print(f"called  {len(line)}")
+                tempText = line.replace('\n','')
+                tempText = tempText.replace('\r','')
+                tempText = tempText.replace('\t', '')
+                iterations = len(line)%pdfLineSize
+                for ix in range(0,iterations):
+                    pdf.cell(pdfLineSize, 3, txt=tempText[int(ix*pdfLineSize):int(((ix+1)*pdfLineSize))], ln=1, align='l')
+            #for only one line
+            else:
+                if('MRN' in line):
+                    pdf.add_page()
+                pdf.cell(pdfLineSize,3, txt=line, ln=1, align='l')
+           # print(line)
+           '''
+
+
+
+
+
+
+
+        pdf.output('testfile1.pdf')
         outfile.close()
 
 
